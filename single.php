0f93b532f39f7ef365728735733245f103729e60
armoula40-max <2.33105127e+08+armoula40-max@users.noreply.github.com>
2026-09-11 16:31:52 +0000

    Match single post card to reference design

--- single.php
+++ single.php
@@ -24,8 +24,11 @@ get_header();
 				$has_howto  = (bool) circlepress_parse_lines( get_post_meta( $post_id, '_cp_howto_steps', true ) );
 				$niche      = circlepress_get_niche( circlepress_get_current_niche() );
 				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cp-single' ); ?>>
-					<div class="cp-single__cat">
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cp-single cp-single--reference' ); ?>>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<figure class="cp-single__thumb cp-single__reference-image"><?php the_post_thumbnail( 'large' ); ?></figure>
+						<?php endif; ?>
+						<div class="cp-single__cat">
 						<?php
 						$cats = get_the_category();
 						foreach ( array_slice( $cats, 0, 3 ) as $cat ) {
@@ -34,7 +37,8 @@ get_header();
 						?>
 					</div>
 					<h1 class="cp-single__title"><?php the_title(); ?></h1>
-					<?php if ( $subtitle ) : ?><p class="cp-single__subtitle"><?php echo esc_html( $subtitle ); ?></p><?php endif; ?>
+						<?php if ( $subtitle ) : ?><p class="cp-single__subtitle"><?php echo esc_html( $subtitle ); ?></p>
+						<?php elseif ( has_excerpt() ) : ?><p class="cp-single__subtitle"><?php echo esc_html( circlepress_clean_excerpt( null, 34 ) ); ?></p><?php endif; ?>
 					<div class="cp-single__meta">
 						<span><?php echo get_avatar( get_the_author_meta( 'ID' ), 28 ); ?> <?php the_author_posts_link(); ?></span>
 						<span><?php echo circlepress_icon( 'calendar', 14 ); ?> <?php echo esc_html( get_the_date() ); ?></span>
@@ -50,11 +54,7 @@ get_header();
 
 					<?php circlepress_ad( 'below_title' ); ?>
 
-					<?php if ( has_post_thumbnail() ) : ?>
-						<figure class="cp-single__thumb"><?php the_post_thumbnail( 'large' ); ?></figure>
-					<?php endif; ?>
-
-					<div class="cp-entry entry-content">
+						<div class="cp-entry entry-content">
 						<?php
 						$content = get_the_content();
 						$content = apply_filters( 'the_content', $content );
